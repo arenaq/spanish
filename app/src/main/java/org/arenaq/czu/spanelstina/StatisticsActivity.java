e914a17f61d42e1c7adbb2a9edc7bc15b8dc9552
Petr Kuška <kuska.petr@gmail.com>
2015-04-09 23:23:59 +0200

    Change statistics option to show content of lectures

--- app/src/main/java/org/arenaq/czu/spanelstina/StatisticsActivity.java
+++ app/src/main/java/org/arenaq/czu/spanelstina/StatisticsActivity.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.TextView;
+
+import org.arenaq.czu.spanelstina.database.Database;
+import org.arenaq.czu.spanelstina.database.model.Lecture;
+import org.arenaq.czu.spanelstina.database.model.Word;
+
+import java.util.List;
 
 /**
  * Created by arenaq on 30.3.2015.
@@ -15,6 +22,23 @@ public class StatisticsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        TextView textView = new TextView(this);
+        setContentView(textView);
+
+        Database db = new Database(this);
+        String text = "";
+        List<Lecture> lectures = db.getAllLectures();
+
+        for (Lecture lecture : lectures) {
+            text += "id=" + lecture.getId() + ",  " + lecture.getName() + ", words={";
+            for (Word word : lecture.getWords()) {
+                text += "[" + word.getId() + "," + word.getSpanish() + "," + word.getCzech() + "],";
+            }
+            text += "}\n";
+        }
+
+        textView.setText(text);
     }
 
 }
