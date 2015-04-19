dc4d7a2424b7cfc3814e35339dd837a90fe26e10
Petr Kuška <kuska.petr@gmail.com>
2015-04-19 21:53:50 +0200

    Remove unused code

--- app/src/main/java/org/arenaq/czu/spanelstina/SettingsActivity.java
+++ app/src/main/java/org/arenaq/czu/spanelstina/SettingsActivity.java
@@ -1,13 +1,12 @@
 package org.arenaq.czu.spanelstina;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 /**
  * Created by arenaq on 15.4.2015.
  */
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +14,4 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         addPreferencesFromResource(R.xml.preferences);
     }
 
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        /*Preference pref = findPreference(key);
-        if (pref instanceof NumberPickerPreference) {
-            NumberPickerPreference npp = (NumberPickerPreference) pref;
-            pref.setSummary(npp.getValue());
-        } else if (pref instanceof ListPreference) {
-            ListPreference lp = (ListPreference) pref;
-            pref.setSummary(lp.getEntry());
-        }*/
-    }
 }
